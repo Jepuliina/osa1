29943c47b5c5665bd1537ef8926e452650d43357
Jepuliina <37802473+Jepuliina@users.noreply.github.com>
2018-05-09 11:35:37 +0300

    Tehtävä 2.12

--- src/index.js
+++ src/index.js
@@ -1,87 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import ShowPersons from './components/showPersons'
-import Filtteri from './components/filtteri'
 import axios from 'axios'
 
-
 class App extends React.Component {
+
     constructor(props) {
         super(props)
         this.state = {
-            persons: [],
-            newName: '',
-            newNumber: '',
+            countries: [],
             filter: '',
         }
     }
 
+
     componentWillMount() {
         axios
-            .get('http://localhost:3001/persons')
+            .get('https://restcountries.eu/rest/v2/all')
             .then(response => {
-                this.setState({persons:response.data})
+                this.setState({ countries: response.data })
             })
     }
 
-    handleNameChange = (event) => {  
-        this.setState({ newName: event.target.value })
-        console.log("Nappia painettu")
-    }
+    ShowCountries = () => {
+        //const filterCountries = () => {
+        //    //return this.state.countries.filter(function (el) {
+        //    //    return el.toLowerCase().indexOf(this.state.filter.toLowerCase()) > -1
 
-    handleNumberChange = (event) => {
-        this.setState({newNumber: event.target.value})
-    }
-
-    handleFilter = (value) => {
-        this.setState({filter: value})
-
-    }
-
-    addName = (event) => {
-        event.preventDefault()
-        const namelist = this.state.persons
-        if (!namelist.find(person => person.name === this.state.newName)) {
-            const nameObject = {
-                name: this.state.newName,
-                number: this.state.newNumber
-            }
-            const names = namelist.concat(nameObject)
-            this.setState({
-                persons: names,
-                newName: "",
-                newNumber: ""
-            })
+        //    //})
+        //}
+        const filtered = this.state.countries.filter(country => country.name.toLowerCase().indexOf(this.state.filter.toLowerCase()) > -1)
+        if (filtered.length > 10) {
+            return (
+                <div>
+                    Too many matches, specify another filter </div>
+            )
+        } else if (filtered.length > 1) {
+            return (
+                <div>
+                    {filtered.map(rivi => <li id={rivi.name}>{rivi.name}</li>)}
+                </div>
+            )
+        } else if (filtered.length === 1) {
+            return (
+                <div>
+                    <h1>{filtered[0].name}</h1>
+                    <p>Capital: {filtered[0].capital}</p>
+                    <p>Population: {filtered[0].population} </p>
+                    <p><img src={filtered[0].flag} alt={filtered[0].name}></img></p>
+                </div>
+            )
         } else {
-            this.setState({ newName: "" })
-            this.setState({ newNumber:""})
-            alert("Nimi on jo luettelossa!")
+            return (
+                <div>
+                No matches </div>
+                )
         }
+    }
+
+    handleChange = (event) => {
+        this.setState({filter: event.target.value})
     }
 
     render() {
         return (
             <div>
-                <h2>Puhelinluettelo</h2>
-                <Filtteri onChange={this.handleFilter.bind(this)} filter={this.state.filter} />
-                <form onSubmit={this.addName}>
-                    <div>
-                        nimi: <input value={this.state.newName} onChange={this.handleNameChange} /><br/>
-                        numero: <input value={this.state.newNumber} onChange={this.handleNumberChange} />
-                    </div>
-                    <div>
-                        <button type="submit">lis&auml;&auml;</button>
-                    </div>
-                </form>
-                <h2>Numerot</h2>
-                <ul>
-                    {this.state.persons.map(person => <ShowPersons key={person.name} person={person} filter={this.state.filter} />)}
-                </ul>
-      </div>
-        )
+                Find countries: <input value={this.state.filter} onChange={this.handleChange} />
+                <this.ShowCountries />
+            </div>
+            )
     }
 }
-
 
 ReactDOM.render(
     <App />,
