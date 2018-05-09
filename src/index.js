16766933aafb1043c183988384797e5508364e22
Jepuliina <37802473+Jepuliina@users.noreply.github.com>
2018-05-09 09:48:01 +0300

    Tehtävä 2.11

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import ShowPersons from './components/showPersons'
 import Filtteri from './components/filtteri'
+import axios from 'axios'
 
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            persons: [
-                { name: 'Arto Hellas', number: '040-123456' },
-                { name: 'Martti Tienari', number: '040-123456' },
-                { name: 'Arto Järvinen', number: '040-123456' },
-                { name: 'Lea Kutvonen', number: '040-123456' }
-            ],
+            persons: [],
             newName: '',
             newNumber: '',
             filter: '',
         }
+    }
+
+    componentWillMount() {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                this.setState({persons:response.data})
+            })
     }
 
     handleNameChange = (event) => {  
